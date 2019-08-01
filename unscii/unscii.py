440980ecc26045388fc1ce07acfd5deabef1e6fd
Grant T. Olson <kgo@grant-olson.net>
2019-08-01 08:30:26 -0400

    Remove debug prints

--- unscii/unscii.py
+++ unscii/unscii.py
@@ -7,9 +7,7 @@ cached_fonts = {}
 class UnsciiFont(object):
     def __init__(self, font_name):
         if font_name not in cached_fonts:
-            print("LAZY LOADING FONT")
             importlib.import_module("unscii.raw_unscii.%s" % font_name, "unscii")
-            print(repr(dir(globals()['raw_unscii'])))
             module = getattr(globals()['raw_unscii'], font_name)
             cached_fonts[font_name] = getattr(module, "%s_bytes" % font_name)
         self.raw_data = cached_fonts[font_name]
